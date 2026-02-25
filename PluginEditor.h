f167e5bf25ba2290b1c0ff0773f2cbb47233cb93
Chris <morenochristopher580@gmail.com>
2026-02-24 18:27:14 -0600

    Update PluginEditor.h

--- PluginEditor.h
+++ PluginEditor.h
@@ -4,8 +4,7 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include "PluginProcessor.h"
 
-class NekoSynthAudioProcessorEditor : public juce::AudioProcessorEditor,
-                                      private juce::Timer
+class NekoSynthAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
     NekoSynthAudioProcessorEditor(NekoSynthAudioProcessor&);
@@ -13,13 +12,13 @@ public:
 
     void paint(juce::Graphics&) override;
     void resized() override;
-    void timerCallback() override;
 
 private:
     NekoSynthAudioProcessor& audioProcessor;
     
     juce::TextButton catButton;
     juce::TextButton dogButton;
+    
     juce::Slider volumeSlider;
     juce::Slider attackSlider;
     juce::Slider decaySlider;
@@ -32,14 +31,11 @@ private:
     juce::Label sustainLabel;
     juce::Label releaseLabel;
     
-    float earWiggle = 0.0f;
-    float tailWag = 0.0f;
-    
-    using Attachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
+    using ButtonAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
     using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
     
-    std::unique_ptr<Attachment> catAttachment;
-    std::unique_ptr<Attachment> dogAttachment;
+    std::unique_ptr<ButtonAttachment> catAttachment;
+    std::unique_ptr<ButtonAttachment> dogAttachment;
     std::unique_ptr<SliderAttachment> volumeAttachment;
     std::unique_ptr<SliderAttachment> attackAttachment;
     std::unique_ptr<SliderAttachment> decayAttachment;
